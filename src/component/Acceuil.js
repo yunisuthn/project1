75184bfd54cb40a71aec22117cc681c283ee6453
yunisuthn <yunis.thn@gmail.com>
2019-03-07 05:13:28 +0300

    ajout

---
+++ src/component/Acceuil.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Acceuil extends Component{
+    render(){
+        return(
+            <h1>Acceuil</h1>
+        )
+    }
+}
+
+export default Acceuil;
